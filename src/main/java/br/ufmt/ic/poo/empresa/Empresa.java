240b71cfe07cfd0745e6c6724edf865feea1ac08
jivagomedeiros <jivago@ic.ufmt.br>
2026-06-09 20:23:30 -0400

    Instância da classe vendedor

--- src/main/java/br/ufmt/ic/poo/empresa/Empresa.java
+++ src/main/java/br/ufmt/ic/poo/empresa/Empresa.java
@@ -1,14 +1,19 @@
 package br.ufmt.ic.poo.empresa;
 
-import br.ufmt.ic.poo.empresa.classes.Funcionario;
+import br.ufmt.ic.poo.empresa.classes.Vendedor;
 
 public class Empresa {
 
     public static void main(String[] args) {
         
-        //Funcionario joao = new Funcionario();
-        //joao.nome = "";
-        
+        Vendedor joao = new Vendedor();
+        joao.setNome("João Silva");
+        joao.setCpf("111.111.111-11");
+        joao.setMatricula("9999");
+        joao.setSetor("Softwares Empresariais");
+        joao.setSalarioBase(3500);
+        joao.setComissao(0.05); //5% de comissão
+        joao.setTotalVendas(100000);
     }
     
 }
